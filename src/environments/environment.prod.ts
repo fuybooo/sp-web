1903b5b6c13d26bb9f5ac6982a0f15cb56cfcd75
fuybooo <fuybooo@qq.com>
2018-06-04 09:33:02 +0800

    部署测试环境配置

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   // project: 'com', // // com gov
   project: 'gov', // // com gov
-  isStatic: false,
-  apiPath: '/uk-bsc/v1', // 不要以斜杠结尾
+  isStatic: true,
+  apiPath: '', // 不要以斜杠结尾
   apiPathChangeable: true,
-  deployPath: '',
+  deployPath: '/dist',
 };
